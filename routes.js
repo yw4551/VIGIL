206468ce62fa3c1b35d83437b376c243c450420d
Yitzi Weiner <yitziw123@gmail.com>
2026-07-07 08:54:42 +0300

    Added all routes in routes.js

---
+++ routes.js
@@ -0,0 +1,42 @@
+const routes = [
+    {
+        method: "GET",
+        path: "/health",
+        handler: checkServerHealth,
+    },
+    {
+        method: "GET",
+        path: "/heroes",
+        handler: getAllHeroes,
+    },
+    {
+        method: "GET",
+        path: "/heroes/:id",
+        handler: getHeroById,
+    },
+    {
+        method: "POST",
+        path: "/heroes",
+        handler: createHero,
+    },
+    {
+        method: "PATCH",
+        path: "/heroes/:id",
+        handler: updateHero,
+    },
+    {
+        method: "DELETE",
+        path: "/heroes/:id",
+        handler: deleteHero,
+    },
+    {
+        method: "POST",
+        path: "/heroes/search",
+        handler: searchHero,
+    },
+    {
+        method: "GET",
+        path: "/stat",
+        handler: getHeroesStat,
+    },
+];
